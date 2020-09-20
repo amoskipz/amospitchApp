3813d505fcc63039fb50cfbad4c0e4fd84b22c86
langat justus <justuslangat78@gmail.com>
2020-09-20 17:35:49 +0300

    crreated routes that gets and add pitch categories

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from flask import render_template, redirect, url_for, abort, request
 from ..models import User, Pitch, Comments, PitchCategory, Votes
 from flask_login import login_required, current_user 
+from .forms import PitchForm, CommentForm, UpdateProfile, CategoryForm
+from .. import db, photos
 
 
 @main.route ('/')
@@ -32,3 +34,31 @@ def new_pitch(id):
         return redirect(url_for('.category', id= category.id))
     
     return render_template('new_pitch.html', pitch_form = form, category = category)    
+
+@main.route ('/categories/<int:id>')
+@login_required
+def category(id):
+    category = PitchCategory.query.get(id)
+    if category is None:
+        abort (404)
+        
+    pitches = Pitch.get_pitches(id)
+    
+    return render_template ('category.html', pitches = pitches, category= category)
+
+
+@main.route('/add/category', methods = ['GET', 'POST'])
+@login_required
+def new_category():
+    form = CategoryForm()
+    
+    if form.validate_on_submit():
+        name = form.name.data
+        new_category = PitchCategory(name=name)
+        new_category.save_category()
+        
+        db.session.add(new_category)
+        db.session.commit()
+        return redirect(url_for('.index'))
+    title = 'New Category'   
+    return render_template('post_comment.html', comment_form = form, title = title)    
