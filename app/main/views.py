3a2ed552e27ef6d44174b7500dc00ca659c7cacd
langat justus <justuslangat78@gmail.com>
2020-09-20 17:34:02 +0300

    created a route to get the new pitch posts

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, redirect, url_for, abort, request
-
+from ..models import User, Pitch, Comments, PitchCategory, Votes
+from flask_login import login_required, current_user 
 
 
 @main.route ('/')
@@ -9,3 +10,25 @@ def index():
     """
     category = PitchCategory.get_categories()
     return render_template('index.html', categories = category)
+
+@main.route('/category/new-pitch/<int:id>', methods= ['GET', 'POST'])
+@login_required
+def new_pitch(id):
+    """
+    Function to fetch data 
+    """
+    form = PitchForm()
+    category = PitchCategory.query.filter_by(id=id).first()
+
+    if category is None:
+        abort(404)
+        
+    if form.validate_on_submit():
+        pitch = form.pitch.data
+        new_pitch = Pitch(pitch = pitch, category_id = category.id, user_id= current_user.id)
+        new_pitch.save_pitch()
+        
+
+        return redirect(url_for('.category', id= category.id))
+    
+    return render_template('new_pitch.html', pitch_form = form, category = category)    
