69ab4d0a3d4a66d0beb2815a07d2cf411e8a1c50
langat justus <justuslangat78@gmail.com>
2020-09-20 16:38:01 +0300

    created a class pitch

--- app/models.py
+++ app/models.py
@@ -22,3 +22,29 @@ class PitchCategory(db.Model):
     def get_categories(cls):
         categories = PitchCategory.query.all()
         return categories
+
+class Pitch(db.Model):
+
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    pitch = db.Column(db.String)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comment = db.relationship("Comments", backref="pitches", lazy = "dynamic")
+    vote = db.relationship("Votes", backref="pitches", lazy = "dynamic")
+
+
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def clear_pitches(cls):
+        Pitch.all_pitches.clear()
+
+
+    def get_pitches(id):
+        pitches = Pitch.query.filter_by(category_id=id).all()
+        return pitches          
