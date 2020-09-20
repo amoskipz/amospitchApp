4aba0909226fb6a043943ae0749385c044108e46
langat justus <justuslangat78@gmail.com>
2020-09-20 17:01:36 +0300

    imported mail and login manager to the init file

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_simplemde import SimpleMDE
 
 def create_app(config_name):
     app = Flask(__name__)
