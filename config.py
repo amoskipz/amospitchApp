d2b990df911a60c8c452fdd7afaa0a25f0824288
langat justus <justuslangat78@gmail.com>
2020-09-20 16:59:20 +0300

    configured the secret key and sqlachemy database uri

--- config.py
+++ config.py
@@ -1,6 +1,24 @@
 import os
 
 class Config:
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    print (SQLALCHEMY_DATABASE_URI, "00000")
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 2435
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    SUBJECT_PREFIX = 'pitch'
+    SENDER_EMAIL = 'justuslangat78@gmail.com'
+    
+# simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
      @staticmethod
     def init_app(app):
         pass
