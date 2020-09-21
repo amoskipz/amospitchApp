8d985351b1c9bdab7ade225b5f95169e835bdc2d
langat justus <justuslangat78@gmail.com>
2020-09-21 08:44:49 +0300

    wrote functions that downvote and upvote on the pitches

--- app/main/views.py
+++ app/main/views.py
@@ -95,3 +95,90 @@ def post_comment(id):
         return redirect(url_for('.view_pitch', id=pitches.id))
 
     return render_template('post_comment.html', comment_form=form, title=title)
+
+@main.route('/user/<uname>')
+@login_required
+def profile(uname):
+    user = User.query.filter_by (username = uname).first()
+    
+    if user is None:
+        abort(404)
+        
+    return render_template('profile/profile.html', user = user)
+
+@main.route('/user/<uname>/update', methods = ['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by (username = uname).first()
+    if user is None:
+        abort(404)
+        
+    form = UpdateProfile()
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        db.session.add(user)
+        db.session.commit()
+        
+        return redirect(url_for('.profile', uname = user.user))
+    return render_template('profile/update.html', form = form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.add(user)
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
+# voting
+@main.route('/pitch/upvote/<int:id>')
+@login_required
+def upvote(id):
+    '''
+    View function that add one to the vote_number column in the votes table
+    '''
+    pitch_id = Pitch.query.filter_by(id=id).first()
+
+    if pitch_id is None:
+         abort(404)
+
+    new_vote = Votes(vote=int(1), user_id=current_user.id, pitches_id=pitch_id.id)
+    new_vote.save_vote()
+    return redirect(url_for('.view_pitch', id=id))
+
+
+
+@main.route('/pitch/downvote/<int:id>')
+@login_required
+def downvote(id):
+    pitch_id = Pitch.query.filter_by(id=id).first()
+
+    new_vote = Votes(vote=int(2), user_id=current_user.id, pitches_id=pitch_id.id)
+    new_vote.save_vote()
+    return redirect(url_for('.view_pitch', id=id))
+
+@main.route('/pitch/downvote/<int:id>')
+@login_required
+def vote_count(id):
+
+    votes = Votes.query.filter_by(user_id=current_user.id).all()
+
+    total_votes = votes.count()
+
+    return total_votes
+
+@main.route('/like/<int:pitch_id>/<action>')
+@login_required
+def like_action(comment_id, action):
+    comment = Comment.query.filter_by(id=post_id).first_or_404()
+    if action == 'vote':
+        current_user.vote_comment(comment)
+        db.session.commit()
+    if action == 'downvote':
+        current_user.unlike_comment(comment)
+        db.session.commit()
+    return redirect(request.referrer)     
